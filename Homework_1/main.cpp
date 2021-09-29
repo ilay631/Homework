bd27ab3fc5f9cefd9b83b4f9c7278577567a23b6
ilay631 <prusskih.ilay@gmail.com>
2021-09-29 19:58:32 +0300

    Добавьте файлы проекта.

---
+++ Homework_1/main.cpp
@@ -0,0 +1,27 @@
+/*
+Level - hard
+*/
+# include <iostream>
+
+
+int add(int num1, int num2) {
+	int res = num1 ^ num2;
+	int shift = num1 & num2;
+	while (shift) {
+		shift <<= 1;
+		int tmp = res;
+		res = tmp ^ shift;
+		shift = tmp & shift;
+	}
+	return res;
+}
+
+
+int main() {
+	using namespace std;
+	int num1, num2;
+	cin >> num1;
+	cin >> num2;
+	cout << add(num1, num2);
+	return 0;
+}
